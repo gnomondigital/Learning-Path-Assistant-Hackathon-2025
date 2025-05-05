959d137ea3feab74da9d3053616494c7d1e5026f
Nacima Yahiatene <nacima.yahiatene@gnomondigital.com>
2025-05-05 18:23:20 +0200

    clean main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,26 +1,11 @@
 import asyncio
 import os
-from typing import Annotated
 
 import semantic_kernel as sk
-from azure.ai.projects.aio import AIProjectClient
-from azure.identity.aio import DefaultAzureCredential
-from semantic_kernel import Kernel
-from semantic_kernel.agents import (AgentGroupChat, AzureAIAgent,
-                                    AzureAIAgentSettings, ChatCompletionAgent,
-                                    ChatHistoryAgentThread)
-from semantic_kernel.agents.strategies import TerminationStrategy
-from semantic_kernel.connectors.ai.chat_completion_client_base import \
-    ChatCompletionClientBase
+from semantic_kernel.agents import ChatCompletionAgent, ChatHistoryAgentThread
 from semantic_kernel.connectors.ai.function_choice_behavior import \
     FunctionChoiceBehavior
-from semantic_kernel.connectors.ai.open_ai import (AzureChatCompletion,
-                                                   OpenAIChatCompletion)
-from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import \
-    AzureChatPromptExecutionSettings
-from semantic_kernel.contents import AuthorRole
-from semantic_kernel.contents.chat_history import ChatHistory
-from semantic_kernel.functions import kernel_function
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.functions.kernel_arguments import KernelArguments
 
 from backend.src.agents.confluence.academy_agent import AcademyAgent
@@ -28,15 +13,9 @@ from backend.src.agents.profile_builder.profile_builder import \
     ProfileBuilderAgent
 from backend.src.agents.web_agent.web_agent import WebAgent
 from backend.src.instructions.instructions_system import GLOBAL_PROMPT
-from backend.src.prompts.academy_instructions import PROMPT as ACADEMY_PROMPT
-from backend.src.prompts.profile_builder import PROMPT as PROFILE_PROMPT
-from backend.src.prompts.search_prompt import PROMPT as WEB_PROMPT
 
 # ---- CONFIGURATION ----
-AGENT_NAME = "GD Academy"
-PROJECT_CONNECTION_STRING = os.environ["PROJECT_CONNECTION_STRING"]
 API_DEPLOYMENT_NAME = os.getenv("MODEL_DEPLOYMENT_NAME")
-BING_CONNECTION_NAME = os.getenv("BING_CONNECTION_NAME")
 AZURE_AI_INFERENCE_API_KEY = os.getenv(
     "AZURE_AI_INFERENCE_API_KEY"
 )
@@ -44,33 +23,9 @@ AZURE_AI_INFERENCE_ENDPOINT = os.getenv(
     "AZURE_AI_INFERENCE_ENDPOINT"
 )
 service_id = "agent"
-# Azure agent settings
-TEMPERATURE = 0.1
-TOP_P = 0.1
-MAX_COMPLETION_TOKENS = 10240
-MAX_PROMPT_TOKENS = 20480
-
-
-class MenuPlugin:
-    """A sample Menu Plugin used for the concept sample."""
-
-    @kernel_function(description="Provides a list of specials from the menu.")
-    def get_specials(self) -> Annotated[str, "Returns the specials from the menu."]:
-        return """
-        Special Soup: Clam Chowder
-        Special Salad: Cobb Salad
-        Special Drink: Chai Tea
-        """
-
-    @kernel_function(description="Provides the price of the requested menu item.")
-    def get_item_price(
-        self, menu_item: Annotated[str, "The name of the menu item."]
-    ) -> Annotated[str, "Returns the price of the menu item."]:
-        return "$9.99"
 
 
 async def main() -> None:
-    service_id = "agent"
     kernel = sk.Kernel()
     kernel.add_service(AzureChatCompletion(
         service_id=service_id,
