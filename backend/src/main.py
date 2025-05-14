7bb0298df2860d27a0e33ec662a8efee249869a5
hela-moalla <hela.moalla@gnomondigital.com>
2025-05-14 09:58:13 +0200

    readme

--- backend/src/main.py
+++ backend/src/main.py
@@ -13,4 +13,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
