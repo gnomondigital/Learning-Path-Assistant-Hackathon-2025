ea57467c766c7e313a3b57acf2fea2d0c5e4c40d
hela-moalla <hela.moalla@gnomondigital.com>
2025-05-15 18:16:50 +0200

    no stream , no history

--- backend/src/agents/confluence/ingestion.py
+++ backend/src/agents/confluence/ingestion.py
@@ -8,9 +8,12 @@ from atlassian import Confluence
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
 from azure.search.documents.indexes import SearchIndexClient
-from azure.search.documents.indexes.models import (SearchableField,
-                                                   SearchFieldDataType,
-                                                   SearchIndex, SimpleField)
+from azure.search.documents.indexes.models import (
+    SearchableField,
+    SearchFieldDataType,
+    SearchIndex,
+    SimpleField,
+)
 from requests.auth import HTTPBasicAuth
 
 from backend.src.agents.confluence.model.base import ConfluencePageModel
@@ -30,9 +33,12 @@ class ConfluenceIngestion:
         self.confluence = Confluence(
             url=self.base_url,
             username=Settings.CONFLUENCE_USERNAME,
-            password=Settings.CONFLUENCE_API_KEY,)
+            password=Settings.CONFLUENCE_API_KEY,
+        )
         self.confluence_content = metadata_store.confluence_content
-        self.AZURE_SEARCH_SERVICE_ENDPOINT = Settings.AZURE_SEARCH_SERVICE_ENDPOINT
+        self.AZURE_SEARCH_SERVICE_ENDPOINT = (
+            Settings.AZURE_SEARCH_SERVICE_ENDPOINT
+        )
         self.AZURE_SEARCH_API_KEY = Settings.AZURE_SEARCH_API_KEY
 
     def fetch_all_pages_from_db(self) -> List[Dict]:
@@ -44,12 +50,15 @@ class ConfluenceIngestion:
         return pages
 
     def fetch_all_pages_from_source(
-            self, space_key: str = "GDA", limit: int = 50) -> List[Dict]:
+        self, space_key: str = "GDA", limit: int = 50
+    ) -> List[Dict]:
 
         pages = self.confluence.get_all_pages_from_space_as_generator(
-            space_key, limit=limit,
+            space_key,
+            limit=limit,
             expand="history,space,version",
-            content_type='page')
+            content_type="page",
+        )
         logger.info("Fetching all pages from Confluence...")
         structured_pages = []
         for page in pages:
@@ -63,7 +72,8 @@ class ConfluenceIngestion:
             version = page["version"]["number"]
             space_key = page["space"]["key"]
             content = self.confluence.get_page_by_id(
-                page_id, expand="body.storage")
+                page_id, expand="body.storage"
+            )
             body = content["body"]["storage"]["value"]
 
             # Store the page data in the metadata store
@@ -84,7 +94,7 @@ class ConfluenceIngestion:
         return structured_pages
 
     def comapre_remote_and_local_content(
-            self, new_pages: List[str], old_pages: List[str]
+        self, new_pages: List[str], old_pages: List[str]
     ) -> List[str]:
         """
         we have a list of the two pages and we want to compare them
@@ -110,16 +120,12 @@ class ConfluenceIngestion:
 
         return pages_to_add, pages_to_update
 
-    async def add_pages_to_local(
-            self, pages_to_add: List[Dict]
-    ) -> None:
+    async def add_pages_to_local(self, pages_to_add: List[Dict]) -> None:
         """
         Add new pages to the metadata store.
         """
         logger.info("Adding new pages to metadata store...")
-        result = self.confluence_content.insert_many(
-            pages_to_add
-        )
+        result = self.confluence_content.insert_many(pages_to_add)
         if result:
             logger.info(
                 f"Added {len(pages_to_add)} new pages to the metadata store."
@@ -127,24 +133,23 @@ class ConfluenceIngestion:
         else:
             logger.error("Failed to add new pages to the metadata store.")
 
-    async def update_pages_in_local(
-            self, pages_to_update: List[Dict]
-    ) -> None:
+    async def update_pages_in_local(self, pages_to_update: List[Dict]) -> None:
         """
         Update existing pages in the metadata store.
         """
         logger.info("Updating existing pages in metadata store...")
         for page in pages_to_update:
             result = self.confluence_content.update_one(
-                {"page_id": page["page_id"]},
-                {"$set": page}
+                {"page_id": page["page_id"]}, {"$set": page}
             )
             if result:
                 logger.info(
-                    f"Updated page {page['id']} in the metadata store.")
+                    f"Updated page {page['id']} in the metadata store."
+                )
             else:
                 logger.error(
-                    f"Failed to update page {page['id']} in the metadata store.")
+                    f"Failed to update page {page['id']} in the metadata store."
+                )
 
     async def update_content_process(self):
         """
@@ -158,8 +163,8 @@ class ConfluenceIngestion:
             new_pages=remote_pages,
         )
 
-        self.add_pages_to_local(pages_to_add=pages_to_add)
-        self.update_pages_in_local(pages_to_update=pages_to_update)
+        await self.add_pages_to_local(pages_to_add=pages_to_add)
+        await self.update_pages_in_local(pages_to_update=pages_to_update)
         logger.info(
             f"""Added {len(pages_to_add)} new pages and\
                 updated {len(pages_to_update)} pages in the metadata store.
@@ -169,5 +174,5 @@ class ConfluenceIngestion:
 
 if __name__ == "__main__":
     ingestion = ConfluenceIngestion()
-    output = asyncio(ingestion.update_content_process())
+    output = asyncio.run(ingestion.update_content_process())
     logger.info("Confluence data extraction and processing completed.")
